fe29ec65de71505dc347982ebed38172356437a9
itishnic <122455719+itishnic@users.noreply.github.com>
2024-07-18 12:22:48 +0300

    commit

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,5 +1,10 @@
 import { deleteContact, fetchContact } from "../../redux/contactsOps";
-import { getContacts, getFilters, selectError, selectIsLoading } from "../../redux/selectors";
+import {
+  getContacts,
+  selectError,
+  selectFilteredContacts,
+  selectIsLoading,
+} from "../../redux/selectors";
 import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css";
 import { useDispatch, useSelector } from "react-redux";
@@ -9,48 +14,36 @@ import { useEffect } from "react";
 const ContactList = () => {
   const dispatch = useDispatch();
 
-  const contacts = useSelector(getContacts) || [];
-  const filters = useSelector(getFilters) || "";
-const isLoading = useSelector(selectIsLoading);
-const error = useSelector(selectError);
-const visibleContacts = useSelector(selectFilteredContacts);
+  const contacts = useSelector(getContacts);
 
-
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+  const visibleContacts = useSelector(selectFilteredContacts);
 
   useEffect(() => {
     dispatch(fetchContact());
   }, [dispatch]);
 
-  const getVisibleContacts =
-    contacts && Array.isArray(contacts)
-      ? contacts.filter((contact) =>
-          contact.name.toLowerCase().includes(filters.toLowerCase())
-        )
-      : [];
-
   const handleDeleteContact = (contactId) => {
     dispatch(deleteContact(contactId));
   };
 
-if (isLoading) {
-  return <p>Loading, please wait...</p>;
-}
+  if (isLoading) {
+    return <p>Loading, please wait...</p>;
+  }
 
-if (error) {
-  return <p>Error: {error}</p>;
-}
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
 
-if (!contacts || contacts.length === 0) {
-  return <p>No contacts found...</p>;
-}
-
-
-
+  if (!contacts || contacts.length === 0) {
+    return <p>No contacts found...</p>;
+  }
 
   return (
     <div>
       <ul className={css.list}>
-        {getVisibleContacts.map((contact) => (
+        {visibleContacts.map((contact) => (
           <li key={contact.id}>
             <Contact
               data={contact}
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,5 +1,5 @@
-export const getContacts = (state) => state.contacts.items;
-export const getFilters = (state) => state.filters.value;
+export const getContacts = (state) => state.contacts.items || [];
+export const getFilters = (state) => state.filters.value || "";
 export const selectIsLoading = (state) => state.contacts.loading;
 export const selectError = (state) => state.contacts.error;
 
@@ -8,10 +8,10 @@ import { createSelector } from "@reduxjs/toolkit";
 
 export const selectFilteredContacts = createSelector(
   [getContacts, getFilters],
-  (contacts, filter) => {
-    const normalizedFilter = filter.toLowerCase();
+  (contacts, filters) => {
+  
     return contacts.filter((contact) =>
-      contact.name.toLowerCase().includes(normalizedFilter)
+      contact.name.toLowerCase().includes(filters.toLowerCase())
     );
   }
 );
